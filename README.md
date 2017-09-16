1b6227517fa387d1a50549693507f2df0ab5ec55
fate0 <fate0@fatezero.org>
2017-09-16 09:52:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# cookiecutter-python
+
+Usage:
+
+```
+cookiecutter https://github.com/fate0/cookiecutter-python
+```
---
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+__email__ = '{{ cookiecutter.author_email }}'
+__author__ = '{{ cookiecutter.author_name }}'
+__version__ = '0.0.1'
